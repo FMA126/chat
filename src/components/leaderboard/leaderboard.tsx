1af8d2fc61636c939f18d6d1e187af66b94a3694
Merritt Blanks <mhblanks@gmail.com>
2023-10-28 17:34:10 -0600

    styling leaderboard

--- src/components/leaderboard/leaderboard.tsx
+++ src/components/leaderboard/leaderboard.tsx
@@ -11,7 +11,7 @@ export const LeaderBoard = () => {
     <GameLayout>
       <div className="p-2 text-white">
         <button
-          className="flex items-center gap-2 rounded-xl border border-white p-2"
+          className="flex items-center gap-2 rounded-xl border border-white bg-white/10 p-2 hover:bg-white/40"
           onClick={() => void router.push("/")}
         >
           <ArrowLeftIcon className="h-4 w-4" />
@@ -20,7 +20,7 @@ export const LeaderBoard = () => {
       </div>
       <div className="flex flex-col items-center text-white">
         <h2 className="text-bold mb-2 text-center text-2xl">Leader Board</h2>
-        <table className="min-w-[50vw] text-center">
+        <table className="min-w-[50vw] rounded-xl bg-white/10 text-center">
           <thead>
             <tr className="">
               <td>Name</td>
@@ -29,7 +29,7 @@ export const LeaderBoard = () => {
           </thead>
           <tbody>
             {leaders.map((user, userIdx) => (
-              <tr key={userIdx} className="border- border">
+              <tr key={userIdx} className="">
                 <td>{user.winnerName}</td>
                 <td>{`${user.countOf}`}</td>
               </tr>
