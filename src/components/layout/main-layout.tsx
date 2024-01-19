116051173ce1907fb409c21c15d3be75c562f22c
Merritt Blanks <mhblanks@gmail.com>
2024-01-19 11:17:49 -0700

    link to reporting issues

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffb1b9] to-[#f26e2b]">
-      {children}
-    </main>
+    <>
+      <div className="flex justify-end bg-[#ffb1b9]">
+        <Link
+          className="pr-2 text-blue-600"
+          href="https://github.com/FMA126/chat/issues"
+          target="_blank"
+        >
+          Report an issue
+        </Link>
+      </div>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffb1b9] to-[#f26e2b]">
+        {children}
+      </main>
+    </>
   );
 }
